18e20ab81aa2a548c3d1fd22f4058fdebedb0d7f
EdinelsonBarros <edinelsonlimabarros@gmail.com>
2025-06-15 22:46:16 -0300

    update

--- src/main/java/formController/FormController.java
+++ src/main/java/formController/FormController.java
@@ -5,17 +5,17 @@ import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 import formDomain.FormValidate;
 
-@RestController
+@Controller
 public class FormController {
 	
 	@GetMapping("/")
 	public String showForm() {		
-		return "home";
+		return "index";
 	}
 	
 	
